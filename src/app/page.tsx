dfd04faac47b8f45bda478878e414379a1840b74
erol-mahmutoglu <erol.mahmutoglu@ilgilen.com.tr>
2023-07-04 00:04:59 +0300

    fixed issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+// import { useRouter } from 'next/navigation';
 import Cookies from 'js-cookie';
 
 import styles from './page.module.css';
 
 export default function Home() {
   const isLoggedIn = Cookies.get('token');
-  const router = useRouter();
+  // const router = useRouter();
 
-  if (isLoggedIn) {
-    router.push('/dashboard');
-  } else {
-    router.push('/login');
-  }
+  // if (isLoggedIn) {
+  //   router.push('/dashboard');
+  // } else {
+  //   router.push('/login');
+  // }
 
   return <main className={styles.main}></main>;
 }
